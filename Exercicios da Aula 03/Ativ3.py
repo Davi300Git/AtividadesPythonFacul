b805320e9fe75fe933c1fd4a74de2d87484fad7e
Davi Sandro dos Santos <93402520+Davi300Git@users.noreply.github.com>
2025-03-18 18:58:08 -0300

    Delete Exercicios da Aula 03/Ativ3.py

--- Exercicios da Aula 03/Ativ3.py
+++
@@ -1,5 +0,0 @@
-# Escreva um programa em Python para calcular o valor de uma prestação em atraso
-#  (prestacao). Para isso, obtenha o valor da prestação (valorPrestacao),
-#  a porcentagem de multa pelo atraso (multa) e a quantidade de dias de atraso 
-# (qtdeDias). Calcular e mostrar o valor da prestação atualizado, sabendo que: 
- #prestacao=valorPrestacao+(valorPrestacao*(multa/100)*qtdeDias)
